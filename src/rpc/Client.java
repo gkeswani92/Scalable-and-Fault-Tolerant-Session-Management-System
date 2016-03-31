3f729f5f2fb889412e7b5e9b54ed9036df9dc3b6
Tanvi Mehta <tanvimmehta@gmail.com>
2016-03-31 19:37:31 -0400

    RPC client write session.

--- src/rpc/Client.java
+++ src/rpc/Client.java
@@ -6,7 +6,7 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Date;
 import java.util.UUID;
 
 public class Client {
@@ -14,28 +14,76 @@ public class Client {
 	public final static int operationSESSIONREAD = 1;
 	public final static int operationSESSIONWRITE = 2;
 	public final static int portProj1bRPC = 5300;
+	public final static String DELIMITER = "_";
+	public final static int TIMEOUT = 300;
+	
 	// 512 bytes
 	public final static int MAX_PACKET_SIZE = 4096;
-	public final static String DELIMITER = "_";
-
 	
-	public static DatagramPacket sessionReadClient(String sessionId, List<String> destIps) {
+	public static DatagramPacket sessionReadClient(String sessionId, int version, String destIp) {
 		try {
 			DatagramSocket rpcSocket = new DatagramSocket();
-			
+			rpcSocket.setSoTimeout(TIMEOUT);
 			
 			// SEND PACKET
 			// Generate unique id for call
 			String callID = UUID.randomUUID().toString();
-			//fill outBuf with [ callID, operationSESSIONREAD, sessionID ]
-			String obuf = callID + DELIMITER + operationSESSIONREAD + DELIMITER +  sessionId;
+			
+			//fill outBuf with [ callID, operationSESSIONREAD, sessionID, version ]
+			String obuf = callID + DELIMITER + operationSESSIONREAD + DELIMITER +  sessionId
+					+ DELIMITER + version;
 			byte[] outBuf = obuf.getBytes();
 			
-			for(String destIp: destIps) {
-			    DatagramPacket sendPkt = new DatagramPacket(outBuf, outBuf.length, InetAddress.getByName(destIp), portProj1bRPC);
-			    rpcSocket.send(sendPkt);
+			DatagramPacket sendPkt = new DatagramPacket(outBuf, outBuf.length, InetAddress.getByName(destIp), portProj1bRPC);
+			rpcSocket.send(sendPkt);
+			
+			// RESPONSE PACKET
+			byte [] inBuf = new byte[MAX_PACKET_SIZE];
+			DatagramPacket recvPkt = new DatagramPacket(inBuf, inBuf.length);
+		    String[] responseParams = null;
+
+			try {
+			    do {
+			      recvPkt.setLength(inBuf.length);
+			      rpcSocket.receive(recvPkt);
+			      if (inBuf != null) {
+				      String response = Arrays.toString(inBuf);
+				      responseParams = response.split(DELIMITER); 
+			      }
+
+			    } while(responseParams == null || !responseParams[0].equals(callID));
+			  } catch(SocketTimeoutException stoe) {
+				  // timeout 
+				  recvPkt = null;
+			  } catch(IOException ioe) {
+				  // other error 
+				  recvPkt = null;
+				  ioe.printStackTrace();
 			  }
 			
+			rpcSocket.close();
+			return recvPkt;
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static boolean sessionWrite(String sessionId, int version, String data, Date discardTime, String destIp) {
+		try {
+			DatagramSocket rpcSocket = new DatagramSocket();
+			
+			// SEND PACKET
+			// Generate unique id for call
+			String callID = UUID.randomUUID().toString();
+			//fill outBuf with [ callID, operationSESSIONWRITE, sessionID, version, data, discard time ]
+			String obuf = callID + DELIMITER + operationSESSIONWRITE + DELIMITER +  sessionId 
+					+ DELIMITER + version + DELIMITER + data + DELIMITER + discardTime;
+			byte[] outBuf = obuf.getBytes();
+			
+			DatagramPacket sendPkt = new DatagramPacket(outBuf, outBuf.length, InetAddress.getByName(destIp), portProj1bRPC);
+			rpcSocket.send(sendPkt);
 			
 			// RESPONSE PACKET
 			byte [] inBuf = new byte[MAX_PACKET_SIZE];
@@ -55,17 +103,21 @@ public class Client {
 			  } catch(SocketTimeoutException stoe) {
 			    // timeout 
 			    recvPkt = null;
+			    rpcSocket.close();
+			    return false;
 			  } catch(IOException ioe) {
 			    // other error 
 			    ioe.printStackTrace();
+			    rpcSocket.close();
+			    return false;
 			  }
 			
 			rpcSocket.close();
-			return recvPkt;
+			return true;
 			
 		} catch(Exception e) {
 			e.printStackTrace();
-			return null;
+			return false;
 		}
 	}
 }
