e12e6eef2063bbcd06040d7376dacc83055a19da
Tanvi Mehta <tanvimmehta@gmail.com>
2016-03-31 19:17:49 -0400

    RPC client - session read.

--- src/rpc/Client.java
+++ src/rpc/Client.java
@@ -4,90 +4,68 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
 import java.net.SocketTimeoutException;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 public class Client {
 	static int count = -1;
+	public final static int operationSESSIONREAD = 1;
+	public final static int operationSESSIONWRITE = 2;
+	public final static int portProj1bRPC = 5300;
+	// 512 bytes
+	public final static int MAX_PACKET_SIZE = 4096;
+	public final static String DELIMITER = "_";
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HELLO Client!");
-		
-		//Random Server IPs///////////////////////////////////////
-		//These are all made up, change the line(on 42) and the for loop(on 38) to make localhost
-		byte[] ipAddr0 = { 127, 0, 0, 1 };
-		byte[] ipAddr1 = { 127, 0, 0, 2 };
-		byte[] ipAddr2 = { 127, 0, 0, 3 };
-		byte[] ipAddr3 = { 127, 0, 0, 4 };
-		ArrayList<byte[]> v = new ArrayList<byte[]>();
-		v.add(ipAddr0);
-		v.add(ipAddr1);
-		v.add(ipAddr2);
-		v.add(ipAddr3);
-		//////////////////////////////////////////////////////////
-		
-		DatagramSocket skt;
+	
+	public static DatagramPacket sessionReadClient(String sessionId, List<String> destIps) {
 		try {
-			skt = new DatagramSocket();
-			int serverSocket = 5300;
+			DatagramSocket rpcSocket = new DatagramSocket();
 			
-			//convert the message to a stream of bytes and store in arr
-			String msg = Integer.toString(count);
-			byte [] arr = msg.getBytes();
 			
-			//send the request to all servers in the ArrayList
-			for (int i = 0; i < v.size(); ++i){
-				//create a DatagramPacket for the respective Server
-				InetAddress host;
-				try {
-					host = InetAddress.getByAddress(v.get(i));
-					DatagramPacket request = new DatagramPacket(arr, arr.length, host, serverSocket);
-					try {
-						//send request
-						skt.send(request);
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				} catch (UnknownHostException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+			// SEND PACKET
+			// Generate unique id for call
+			String callID = UUID.randomUUID().toString();
+			//fill outBuf with [ callID, operationSESSIONREAD, sessionID ]
+			String obuf = callID + DELIMITER + operationSESSIONREAD + DELIMITER +  sessionId;
+			byte[] outBuf = obuf.getBytes();
 			
-			//now recv the message
-			byte [] arr_rcv = new byte[512];
-			DatagramPacket recvPkt = new DatagramPacket(arr_rcv, arr_rcv.length);
+			for(String destIp: destIps) {
+			    DatagramPacket sendPkt = new DatagramPacket(outBuf, outBuf.length, InetAddress.getByName(destIp), portProj1bRPC);
+			    rpcSocket.send(sendPkt);
+			  }
 			
-			try{
-				do{
-					recvPkt.setLength(arr_rcv.length);
-					skt.receive(recvPkt);
-				}while (new String(recvPkt.getData()) != "-1");
-				skt.close();
-			}
-			catch(SocketTimeoutException stoe) {
+			
+			// RESPONSE PACKET
+			byte [] inBuf = new byte[MAX_PACKET_SIZE];
+			DatagramPacket recvPkt = new DatagramPacket(inBuf, inBuf.length);
+		    String[] responseParams = null;
+
+			try {
+			    do {
+			      recvPkt.setLength(inBuf.length);
+			      rpcSocket.receive(recvPkt);
+			      if (inBuf != null) {
+				      String response = Arrays.toString(inBuf);
+				      responseParams = response.split(DELIMITER); 
+			      }
+
+			    } while(responseParams == null || !responseParams[0].equals(callID));
+			  } catch(SocketTimeoutException stoe) {
 			    // timeout 
 			    recvPkt = null;
-			} 
-			catch(IOException ioe) {
+			  } catch(IOException ioe) {
 			    // other error 
-			}
-			catch(Exception ex){
-				System.out.println("EXCEPTION CAUGHT");
-			}
+			    ioe.printStackTrace();
+			  }
 			
-			skt.close();
-		} catch (SocketException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			rpcSocket.close();
+			return recvPkt;
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
 		}
-		//skt.receive(reply);
-		//System.out.println("Client Received: "+ new String(reply.getData()));
-		
 	}
-
 }
