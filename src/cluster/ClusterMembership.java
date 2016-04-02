97e1d1e955f20acd65424aa1b29b42ab1e5fee0d
gkeswani92 <gauravkeswani92@gmail.com>
2016-04-02 08:26:36 -0400

    Cluster membership prober

---
+++ src/cluster/ClusterMembership.java
@@ -0,0 +1,21 @@
+package cluster;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ClusterMembership {
+	
+	private static Map<String,List<String>> membership;
+	
+	public List<String> getMemberIPAddress(){
+		List<String> ipAddr = new ArrayList<String>();
+		
+		for(String amiIndex: membership.keySet()){
+			String currentIP = membership.get(amiIndex).get(0);
+			ipAddr.add(currentIP);
+		}
+		
+		return ipAddr;
+	}
+}
